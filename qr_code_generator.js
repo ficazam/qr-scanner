e6fbf3d347a5b697597d355c77329857d77336f4
ficazam <felipeicaza@gmail.com>
2024-12-14 11:00:26 -0500

    fixes

--- qr_code_generator.js
+++ qr_code_generator.js
@@ -1,7 +1,7 @@
 const qr = require("qrcode");
 
 function createQrCode() {
-  qr.toFile("qr_code.png", "http://192.168.1.39:3000", function (err) {
+  qr.toFile("qr_code.png", "https://qr-scanner-h9agjc1q8-ficazams-projects.vercel.app/", function (err) {
     if (err) return console.error(err);
     console.log("QR code generated and saved.");
   });
